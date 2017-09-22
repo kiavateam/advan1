618e2eec90b0b6012d5b2439221063f89e973993
kiavateam <ava@kiava.ir>
2017-09-22 23:22:32 +0330

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,19 @@
+do local _ = {
+  Helper = "helpertagsudobot",
+  Token = "391510198:AAFSFcBW-tph4zY8LJlwztPaJ7mgpQBycT4",
+  bot_owner = {
+    392532814
+  },
+  cmd = "^[/!#]",
+  license = "advanplus-H137",
+  sudo_users = {
+    253326696,
+    275027926,
+    282958812,
+    261764158,
+    150007930,
+    340865871
+  }
+}
+return _
+end
